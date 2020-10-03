58cd8dc42c7ff5309e5e6eb7024ad6ac8ddaee25
Johnson Zhong <zhsh@umich.edu>
2020-10-03 19:11:39 -0400

    Point to github URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='window_recorder',
     version='0.1.0',
     packages=['window_recorder'],
-    url='',
+    url='https://github.com/LemonPi/window_recorder',
     license='MIT',
     author='zhsh',
     author_email='zhsh@umich.edu',
